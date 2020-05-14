b0bff481f00b8d80359cd3ee037262ba42e81487
x161REB134 <sakovics.ricards@gmail.com>
2020-05-14 23:03:38 +0300

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,11 +1,24 @@
 import socket
+import sys
 
-HOST = '0.0.0.0'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+if len(sys.argv) == 3:
+    # Get "IP address of Server" and also the "port number" from argument 1 and argument 2
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+else:
+    print("Run like : python3 client.py <arg1 server ip 192.168.1.102> <arg2 server port 4444 >")
+    exit(1)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+# Create socket for server
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+print("Do Ctrl+c to exit the program !!")
 
-print('Received', repr(data))
+# Let's send data through UDP protocol
+while True:
+    send_data = input("Type some text to send =>");
+    s.sendto(send_data.encode('utf-8'), (ip, port))
+    print("\n\n 1. Client Sent : ", send_data, "\n\n")
+    data, address = s.recvfrom(4096)
+    print("\n\n 2. Client received : ", data.decode('utf-8'), "\n\n")
+# close the socket
+s.close()
